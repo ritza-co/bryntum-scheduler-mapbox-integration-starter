379ffd1b04cb25023670f13be167f0d38f4fe28b
Matthew Clarke <m_d_clarke@hotmail.com>
2023-02-06 10:18:35 +0200

    added completed scheduler

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+import { Splitter, Toast } from "@bryntum/schedulerpro";
 import Schedule from "./lib/Schedule.js";
 import Task from "./lib/Task.js";
+import MapPanel from "./lib/MapPanel.js";
+
+const detectWebGL = () => {
+  try {
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    const supported = Boolean(canvas.getContext("webgl"));
+    canvas.remove();
+    return supported;
+  } catch (e) {
+    return false;
+  }
+};
+
+let mapPanel;
 
 const schedule = new Schedule({
   ref: "schedule",
@@ -23,4 +39,51 @@ const schedule = new Schedule({
     // It's meant to be used as a development stage helper only so please set it to false for production systems.
     validateResponse: true,
   },
+  listeners: {
+    eventClick: ({ eventRecord }) => {
+      // When an event bar is clicked, bring the marker into view and show a tooltip
+      if (eventRecord.marker) {
+        mapPanel?.showTooltip(eventRecord, true);
+      }
+    },
+    afterEventSave: ({ eventRecord }) => {
+      if (eventRecord.marker) {
+        mapPanel?.scrollMarkerIntoView(eventRecord);
+      }
+    },
+  },
 });
+
+// A draggable splitter between the two main widgets
+new Splitter({
+  appendTo: "main",
+});
+
+if (detectWebGL()) {
+  // A custom MapPanel wrapping the MapboxGL JS map. We provide it with the timeAxis and the eventStore
+  // So the map can show the same data as seen in the schedule
+  mapPanel = new MapPanel({
+    ref: "map",
+    appendTo: "main",
+    flex: 2,
+    eventStore: schedule.eventStore,
+    timeAxis: schedule.timeAxis,
+    listeners: {
+      // When a map marker is clicked, scroll the event bar into view and highlight it
+      markerclick: async ({ eventRecord }) => {
+        await schedule.scrollEventIntoView(eventRecord, {
+          animate: true,
+          highlight: true,
+        });
+        schedule.selectedEvents = [eventRecord];
+      },
+    },
+  });
+} else {
+  Toast.show({
+    html: `ERROR! Cannot show map. WebGL is not supported!`,
+    color: "b-red",
+    style: "color:white",
+    timeout: 0,
+  });
+}
